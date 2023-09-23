9ac3c5f5da37d538e4e6b00d0eadab31ab2a2499
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2023-09-23 14:29:58 +0300

    Add files via upload

--- Бинарный поиск/BinSearch.py
+++ Бинарный поиск/BinSearch.py
@@ -1,41 +1,35 @@
-def binary_search(list_num , to_search):
+def binary_search_manual_input():
+    # Запросите у пользователя ввод массива
+    list_str = input("Введите отсортированный массив чисел через пробел: ")
+    list_num = [int(x) for x in list_str.split()]
+
+    # Запросите у пользователя ввод индекса, который нужно найти
+    to_search = int(input("Введите число, которое нужно найти: "))
+
     first_index = 0
     size = len(list_num)
     last_index = size - 1
     mid_index = (first_index + last_index) // 2
-    # print(mid_index)
-    mid_element = list_num[mid_index]
-    # print(mid_element)
 
     is_found = True
     while is_found:
         if first_index == last_index:
-            if mid_element != to_search:
+            if list_num[mid_index] != to_search:
                 is_found = False
-                return " Does not appear in the list"
+                return f"-1"
 
-        elif mid_element == to_search:
-            return f"{mid_element} occurs in position {mid_index}"
+        elif list_num[mid_index] == to_search:
+            return f"{to_search} встречается в позиции {mid_index}"
 
-        elif mid_element > to_search:
-            new_position = mid_index - 1
-            last_index = new_position
+        elif list_num[mid_index] > to_search:
+            last_index = mid_index - 1
             mid_index = (first_index + last_index) // 2
-            mid_element = list_num[mid_index]
-            if mid_element == to_search:
-                return f"{mid_element} occurs in position {mid_index}"
 
-        elif mid_element < to_search:
-            new_position = mid_index + 1
-            first_index = new_position
-            last_index = size - 1
+        elif list_num[mid_index] < to_search:
+            first_index = mid_index + 1
             mid_index = (first_index + last_index) // 2
-            mid_element = list_num[mid_index]
-            if mid_element == to_search:
-                return f"{mid_element} occurs in position {mid_index}"
 
-
-
-list_container = [16 , 18 , 20 , 50 , 60 , 81 , 84 , 89]
-print(binary_search(list_container , 81))
-print(binary_search(list_container , 10))
+    return f"{to_search} не встречается в списке"
+# Вызов функции для поиска с вводом от пользователя
+result = binary_search_manual_input()
+print(result)
